c57848052e87a0f48ee6f26348d826d4f4e4bb0e
Paulo Trentin <paulo@paulotrentin.com.br>
2020-12-11 23:50:32 -0300

    Added support to flip and flop on sprite animations

--- src/gameObjects/sprite.js
+++ src/gameObjects/sprite.js
@@ -1,3 +1,4 @@
+import { unitToPx } from "../metric"
 
 function Sprite(spriteSheet, x, y, width, height, timePerFrameInMs, numberOfFrames) {
     this.spriteSheet = spriteSheet
@@ -7,12 +8,14 @@ function Sprite(spriteSheet, x, y, width, height, timePerFrameInMs, numberOfFram
     this.height = height
     this.timePerFrameInMs = timePerFrameInMs
     this.numberOfFrames = numberOfFrames
-
+    this.flip = false
+    this.flop = false
 
     this.frameIndex = 0
     this.lastUpdate = Date.now()
 
-    this.update = () => {
+    this.play = (canvasContext) => {
+        canvasContext.save();
 
         if(Date.now() - this.lastUpdate >= this.timePerFrameInMs) {
             this.frameIndex++;
@@ -23,21 +26,40 @@ function Sprite(spriteSheet, x, y, width, height, timePerFrameInMs, numberOfFram
 
             this.lastUpdate = Date.now();
         }
-    }
 
+        let flipScale, flopScale = 1;
+    
+        canvasContext.save()
+        
+        if(this.flip) {
+            flipScale = -1; 
+            canvasContext.translate(this.width / this.numberOfFrames, 0);
+        } else {
+            flipScale = 1;
+        }
+        
+        if(this.flop) {
+            flopScale = -1; 
+            canvasContext.translate(0, this.width / this.numberOfFrames);
+        } else {
+            flopScale = 1;
+        }
 
-    this.draw = function (context) {
-      context.drawImage(
-        this.spriteSheet,
-        (this.frameIndex * this.width) / this.numberOfFrames,
-        0,
-        this.width / this.numberOfFrames,
-        this.height,
-        x,
-        y,
-        this.width / this.numberOfFrames,
-        this.height
-      );
+        canvasContext.scale(flipScale, flopScale);
+
+        canvasContext.drawImage(
+            this.spriteSheet,
+            (this.frameIndex * this.width) / this.numberOfFrames,
+            0,
+            this.width / this.numberOfFrames,
+            this.height,
+            unitToPx(this.x) * flipScale,
+            unitToPx(this.y) * flopScale,
+            this.width / this.numberOfFrames,
+            this.height
+        );
+
+        canvasContext.restore();
     };
 
 }
